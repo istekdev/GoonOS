54881e90cea7c96ccd2d2fa2be3e15deb48edcfa
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-11-13 08:52:21 -0600

    Create calculator.py

---
+++ system/source/apps/calculator.py
@@ -0,0 +1,19 @@
+from .system.gui.apps import calculator
+import math
+
+input = calculator.input
+
+def trig(type):
+  radian = math.radian(int(calculator.input))
+  if type.lower() == "sin":
+    return math.sin(radian)
+  elif type.lower() == "cos":
+    return math.cos(radian)
+  elif type.lower() == "tan":
+    return math.tan(radian)
+
+def pi():
+  return math.pi
+
+def calc():
+  return eval(input)
